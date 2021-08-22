bfdc9367fcd47cfcc2aff7eb5b20a0070bc726fb
Patrick Kalkman <patrick.kalkman@exlrt.com>
2021-08-22 20:20:32 +0200

    module4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import { fetchWithTimeout } from './services';
-const movies = require('./data/movies.json');
+import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
 
-export function fetchMovies() {
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()]).then(([books, movies ]) => ({
+        books,
+        movies
+      }))
+      .catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
+const getBooksAndMoviesPromise = getBooksAndMovies();
 
-moviePromise.then((results) => {
-    console.log(results);
+getBooksAndMoviesPromise.then((results) => {
+    console.log('getBooksAndMoviesPromise', result);
 });
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()]).then(results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+
+getBooksOrMoviesPromise.then((results) => {
+    console.log('getBooksOrMoviesPromise', result);
+}); 
